bf07fb988ff62e6771b2bc6c251face6aaa5beb5
Dogac Akyildiz <dogac.akyildiz@bilgeadam.com>
2019-03-07 03:50:51 +0300

    Project committed

---
+++ DogacProject/Controllers/StudentsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DogacProject.Models;
+
+namespace DogacProject.Controllers
+{
+    public class StudentsController : Controller
+    {
+        DogacContext DogacContext;
+
+        public StudentsController(DogacContext context)
+        {
+            DogacContext = context;
+        }
+
+        public IActionResult Index()
+        {
+            var students = DogacContext.Students.ToList();
+            return View(students);
+        }
+
+        public IActionResult Detay(int id)
+        {
+
+            Student student = DogacContext.Students.Where(s => s.Id == id).FirstOrDefault();
+            if (student != null)
+            {
+                return View(student);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ DogacProject/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogacProject.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public long StuID { get; set; }
+    }
+}
