c108a524f5e5075314d2eac40b2b49a92deafc67
Dogac Akyildiz <dogac.akyildiz@bilgeadam.com>
2019-04-12 00:12:14 +0300

    Logout Button is Fixed.

--- DogacProject/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ DogacProject/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -23,14 +23,16 @@ namespace DogacProject.Areas.Identity.Pages.Account
             _logger = logger;
         }
 
-        public void OnGet()
+        public async Task<IActionResult> OnGet(string returnUrl = null)
         {
+            await _signInManager.SignOutAsync();
+            _logger.LogInformation("User logged out.");
+            return LocalRedirect("~/");
         }
 
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
-            await _signInManager.SignOutAsync();
-            _logger.LogInformation("User logged out.");
+
             if (returnUrl != null)
             {
                 return LocalRedirect(returnUrl);
